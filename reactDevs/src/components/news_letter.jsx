8ef30842e8bb0144d3e2bc48da5bdeb0a941b279
T-Rahul-prabhu-38 <trahulprabhu38@gmail.com>
2024-06-03 01:04:36 +0530

    project2 has started, learning hooks

--- reactDevs/src/components/news_letter.jsx
+++ reactDevs/src/components/news_letter.jsx
@@ -5,10 +5,10 @@ const NewsList = (props) => {
     const newsHandler = props.news.map( item =>(
        <NewsListItem key={item.id} item={item}/>
     ));
-    console.log("hello");
     return(
         <>
             { newsHandler }
+            {props.children}
         </>
     )
 }
