57f66cf8a1fdc1b1591cdc77976a1793dd88063f
T-Rahul-prabhu-38 <trahulprabhu38@gmail.com>
2024-06-05 12:18:23 +0530

    bad cmt

--- reactProps/src/App.jsx
+++ reactProps/src/App.jsx
@@ -1,16 +1,67 @@
+import { useState } from "react";
+
+import {CORE_CONCEPTS} from "./data";
+import { EXAMPLES } from "./data";
+import Header from "./components/header";
+import CoreConcepts from "./components/coreconcepts";
+import TabButton from "./components/buttons";
+import { label } from "three/examples/jsm/nodes/Nodes.js";
+
+
 function App() {
+
+  const[tabContent,settabContent] = useState();
+
+  function selectHandler(label){
+    console.log(label);
+    
+    settabContent(label);
+  }
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
+        <section id="core-concepts">
         <h2>Time to get started!</h2>
+        <ul>
+          {/* {CORE_CONCEPTS.map((conceptItem)=>{
+              <CoreConcepts {...conceptItem} />
+          })} */}
+          <CoreConcepts
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}  
+            image = {CORE_CONCEPTS[0].image}
+          />
+          <CoreConcepts {... CORE_CONCEPTS[1]}/>
+          <CoreConcepts {...CORE_CONCEPTS[2]}/>
+          <CoreConcepts {...CORE_CONCEPTS[3]}/>
+        </ul>
+        </section>
+        <section id="examples">
+          <h2>examples</h2>
+          <menu>
+            <TabButton isSelected={tabContent==='components'} onSelect={()=>selectHandler('components')}>components</TabButton>
+            <TabButton isSelected={tabContent==='jsx'} onSelect={()=>selectHandler('jsx')}>JSX</TabButton>
+            <TabButton isSelected={tabContent==='props'} onSelect={()=>selectHandler('props')}>Props</TabButton>
+            <TabButton isSelected={tabContent==='state'} onSelect={()=>selectHandler('state')}>State</TabButton>
+          </menu>
+          <div id="tab-content">
+            {!tabContent ? 
+              <p>Eplore now!</p> 
+              :
+              <>
+                 <h3>{EXAMPLES[tabContent].title}</h3>
+              <p>{EXAMPLES[tabContent].description}</p>
+                <pre>
+                  <code>
+                    {EXAMPLES[tabContent].code}
+                  </code>
+                </pre>
+              </>
+            
+            } 
+          </div>
+        </section>
       </main>
     </div>
   );
